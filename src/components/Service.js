c2f20e604970b74d5dc8b4505dfd7b36202f35f0
WilliamRMoore <williamrmoore93@gmail.com>
2020-12-05 13:44:52 -0500

    removed service links

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,16 +1,16 @@
 import React from "react"
 import Image from "gatsby-image"
-import { Link } from "gatsby"
+//import { Link } from "gatsby"
 const Service = ({id, title, image, slug, description}) => {
-  return <Link to={`/services/${slug}`} key={id} className="blog">
-    <article>
+  // return <Link to={`/services/${slug}`} key={id} className="blog">
+    return <article>
       <Image fluid={image.fluid} className="blog-img"/>
       <div className="blog-card">
         <h4>{title}</h4>
         <p>{description.description}</p>
       </div>
     </article>
-  </Link>
+  {/* </Link> */}
 }
 
 
